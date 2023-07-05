4e834c303b5649be45a835b3cad43c59961a097a
Rocky Haque <rockyhaque99@gmail.com>
2023-07-05 09:32:50 +0600

    Create group_function.sql

---
+++ group_function.sql
@@ -0,0 +1,16 @@
+SELECT * FROM employees;
+
+SELECT MAX(salary)
+FROM employees;
+
+SELECT MIN(salary)
+FROM employees;
+
+SELECT SUM(salary)
+FROM employees;
+
+SELECT AVG(salary)
+FROM employees;
+
+SELECT COUNT(salary)
+FROM employees;
